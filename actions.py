e54a7db6fdda1775232f09d0bb0c12cc12240f4e
Rafael Moreira <rafael.goesam@gmail.com>
2024-09-30 19:27:59 -0300

    feat: Update Map and create Tiles

--- actions.py
+++ actions.py
@@ -1,15 +1,31 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
 
 class Action:
     """
     Class that governs an action in general
     """
-    # pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        """Perform this action with the objects needed to determine its scope.
+        `engine` is the scope this action is being performed in.
+        `entity` is the object performing the action.
+        This method must be overridden by Action subclasses.       
+        """
+        raise NotImplementedError()
 
 
 class EscapeAction(Action):
     """
     Click of the esc button
     """
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
     # pass
 
 
@@ -22,3 +38,13 @@ class MovementAction(Action):
 
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination is out of bounds.
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination is blocked by a tile.
+        entity.move(self.dx, self.dy)
